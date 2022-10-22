2eb1c712ec86c4bf93b0b7223865ca70901c6470
Ezequiel Bosco <ezequiel_bosco@hotmail.com>
2022-10-22 03:10:35 -0300

    creo template lista_de_personas

--- ejemplo/views.py
+++ ejemplo/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
+from ejemplo.models import Persona
 
-# Create your views here.
+class ListarPersonas(View):
+    template_name = "ejemplo/lista_de_personas.html"
+
+    def get(self, request):
+        personas = Persona.objects.all()
+        return render(request, self.template_name, {"personas":personas})
--- proyecto/urls.py
+++ proyecto/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from ejemplo.views import ListarPersonas
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('personas/', ListarPersonas.as_view()),
 ]
